d27ed28e9bb7581617ad31f500867043677b7261
Hemant Pande <hemant.pande87@outlook.com>
2019-02-11 12:14:51 +0530

    Initial scaffolding and folder structure

---
+++ First-meetup/Code-samples/PeopleSoftBank/AccountBuddy.Domain.Tests/AccountTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AccountBuddy.Domain.Tests
+{
+    [TestClass]
+    public class AccountTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
