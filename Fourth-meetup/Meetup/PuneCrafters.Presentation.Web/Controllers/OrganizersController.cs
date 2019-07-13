c125eb9b882a02219a88b6a9a370f128b9226aa1
Dattatray Kale <way2datta@gmail.com>
2019-07-13 16:29:53 +0530

    Meetup Scheduler added

---
+++ Fourth-meetup/Meetup/PuneCrafters.Presentation.Web/Controllers/OrganizersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PuneCrafters.Presentation.Web.Controllers
+{
+    public class OrganizersController : Controller
+    {
+        public ActionResult Schedule()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Schedule(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
