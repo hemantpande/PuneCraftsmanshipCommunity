8600c9ee88e93eadb68ff7d085471fb8ce4022d1
Dattatray Kale <way2datta@gmail.com>
2019-07-17 20:09:14 +0530

    Increased cyclomatic complexity, Jim Sir. Cheers enjoy your evening with your drinks!

--- Fourth-meetup/Meetup/Services/Scheduler.cs
+++ Fourth-meetup/Meetup/Services/Scheduler.cs
@@ -6,31 +6,37 @@ namespace Services
     {
         public void ScheduleMeetup(User user, string topic, string description, DateTime date, uint maxParticipants, int locationId)
         {
-            if (user.Plan != MembershipPlan.Gold && maxParticipants > 10)
+            if (!string.IsNullOrWhiteSpace(topic))
             {
-                if (user.Plan != MembershipPlan.Silver)
+                if (!string.IsNullOrWhiteSpace(description))
                 {
-                    throw new Exception("Free plan can only have up to 10 participants.");
+                    if (date >= DateTime.Today)
+                    {
+                        if (user.Plan != MembershipPlan.Gold && maxParticipants > 10)
+                        {
+                            if (user.Plan != MembershipPlan.Silver)
+                            {
+                                throw new Exception("Free plan can only have up to 10 participants.");
+                            }
+                            else if (maxParticipants > 50)
+                            {
+                                throw new Exception("Silver plan can only have up to 50 participants.");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Meetup must be tomorrow or later.");
+                    }
                 }
-                else if (maxParticipants > 50)
+                else
                 {
-                    throw new Exception("Silver plan can only have up to 50 participants.");
+                    throw new ArgumentException("Description is required.");
                 }
             }
-
-            if (string.IsNullOrWhiteSpace(topic))
+            else
             {
                 throw new ArgumentException("Topic is required.");
-            }
-
-            if (string.IsNullOrWhiteSpace(description))
-            {
-                throw new ArgumentException("Description is required.");
-            }
-
-            if (date < DateTime.Today)
-            {
-                throw new ArgumentException("Meetup must be tomorrow or later.");
             }
 
             if (locationId == 0)
