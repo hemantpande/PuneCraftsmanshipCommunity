1374483dba8595c3608a3b1b95abfadf46eb5c38
Dattatray Kale <way2datta@gmail.com>
2019-07-20 16:37:14 +0530

    Registration is made ugliest of all time, Cheers!

--- Fourth-meetup/Meetup/Services/Registration.cs
+++ Fourth-meetup/Meetup/Services/Registration.cs
@@ -17,37 +17,44 @@ namespace Services
                 {
                     mUp = new MeetupData().Get(meetup);
                     var ok = true;
-                    if (mUp.Date < DateTime.Now)
+
+                    if (mUp.Date > DateTime.Now)
+                    {
+                        if (mUp.Date < DateTime.Today.AddDays(10) ||
+                            (mUp.Date < DateTime.Today.AddDays(30) && user.Plan == MembershipPlan.Silver) ||
+                            user.Plan == MembershipPlan.Gold
+                            )
+                        {
+                            var meetupLocation = new LocationData().GetLocation(meetup);
+                            int travelDistance = new LocationData().FindDistance(user.LocationId, meetupLocation);
+
+                            new MeetupData().AddParticipant(meetup, user.Id, travelDistance);
+                        }
+                        else if (mUp.Date > DateTime.Today.AddDays(10))
+                        {
+                            if (user.Plan == MembershipPlan.Free)
+                            {
+                                throw new Exception("You can sign up for meetups only in the last 10 days before start. Upgrade otherwise.");
+                            }
+                        }
+                        else if (mUp.Date > DateTime.Today.AddDays(30))
+                        {
+                            if (user.Plan == MembershipPlan.Silver)
+                            {
+                                throw new Exception("You can sign up for meetups only in the last 30 days before start. Upgrade otherwise.");
+                            }
+                        }
+                    }
+                    else
                     {
                         throw new Exception("Past meetup.");
                     }
-                    else if (mUp.Date > DateTime.Today.AddDays(10))
-                    {
-                        if (user.Plan == MembershipPlan.Free)
-                        {
-                            throw new Exception("You can sign up for meetups only in the last 10 days before start. Upgrade otherwise.");
-                        }
-                    }
-                    else if (mUp.Date > DateTime.Today.AddDays(30))
-                    {
-                        if (user.Plan == MembershipPlan.Silver)
-                        {
-                            throw new Exception("You can sign up for meetups only in the last 30 days before start. Upgrade otherwise.");
-                        }
-                    }
-
-                    var meetupLocation = new LocationData().GetLocation(meetup);
-                    int travelDistance = new LocationData().FindDistance(user.LocationId, meetupLocation);
-
-                    new MeetupData().AddParticipant(meetup, user.Id, travelDistance);
                 }
                 else
                 {
                     throw new ArgumentNullException("user");
-
                 }
             }
-        
             else
             {
                 throw new ArgumentException("Meetup is required.");
