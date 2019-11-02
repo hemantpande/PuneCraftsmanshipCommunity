fc980b4db65ae946eaf526b807375b0b7f11171a
Dattatray Kale <way2datta@gmail.com>
2019-11-02 09:18:26 +0530

    Dirtier code example for fifth meetup

---
+++ Fifth-Meetup/MovieRental.ClassLibrary/Utils.cs
@@ -0,0 +1,124 @@
+﻿namespace MovieRental.ClassLibrary
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class CustInfo
+    {
+        private String cust_name;
+        private List<RentalDetails> rentals = new List<RentalDetails>();
+
+        public CustInfo(String name)
+        {
+            this.cust_name = name;
+        }
+
+        public void addRental(RentalDetails arg)
+        {
+            rentals.Add(arg);
+        }
+
+        public String getCustName()
+        {
+            return cust_name;
+        }
+
+        public String statement()
+        {
+            double temp = 0;
+            int points = 0;
+            String result = "Rental Record for " + getCustName() + "\n";
+            foreach (RentalDetails rd in rentals)
+            {
+                double amt = 0;
+
+                switch (rd.getMovie().getPriceCode())
+                {
+                    case 0: // Regular movie hai toh?
+                        amt += 2;
+                        if (rd.getDaysRented() > 2)
+                            amt += (rd.getDaysRented() - 2) * 1.5;
+                        break;
+
+                    case 1: // You know it's a Newly released movie?
+                        amt += rd.getDaysRented() * 3;
+                        break;
+
+                    case 2: // O Sharmaji, childrens ke liye kuch plan nahi hai kya?
+                        amt += 1.5;
+                        if (rd.getDaysRented() > 3)
+                            amt += (rd.getDaysRented() - 3) * 1.5;
+                        break;
+                }
+
+                // add frequent renter points
+                points++;
+                
+                // add bonus for a two day new release rental
+                if ((rd.getMovie().getPriceCode() == 1)
+                        &&
+                        rd.getDaysRented() > 1) points++;
+
+                //show figures for this rental
+                result += "\t" + rd.getMovie().getMovieTitle() + "\t" +
+                        amt + "\n";
+                temp += amt;
+            }
+
+            //add footer lines result
+            result += "Amount owed is " + temp + "\n";
+            result += "You earned " + points
+                    + " frequent renter points";
+            return result;
+        }
+    }
+
+    public class RentalDetails
+    {
+        private int daysRented;
+        private MovieData movie;
+
+        public RentalDetails(MovieData movie, int daysRented)
+        {
+            this.movie = movie;
+            this.daysRented = daysRented;
+        }
+
+        public int getDaysRented()
+        {
+            return daysRented;
+        }
+
+        public MovieData getMovie()
+        {
+            return movie;
+        }
+    }
+
+    public class MovieData
+    {
+        private String movieTitle;
+        private int priceCode;
+
+        public MovieData(String title, int priceCode)
+        {
+            this.movieTitle = title;
+            this.priceCode = priceCode;
+        }
+
+        public int getPriceCode()
+        {
+            return priceCode;
+        }
+
+        public void setPriceCode(int arg)
+        {
+            priceCode = arg;
+        }
+
+        public String getMovieTitle()
+        {
+            return movieTitle;
+        }
+    }
+}
--- Fifth-Meetup/MovieRental.Tests/MainTest.cs
+++ Fifth-Meetup/MovieRental.Tests/MainTest.cs
@@ -5,25 +5,25 @@ using MovieRental.ClassLibrary;
 namespace MovieRental.Tests
 {
     [TestClass]
-    public class CustomerTest
+    public class MainTest
     {
         [TestMethod]
         public void Customer_GenerateTextStatement() 
         {
-            Customer customer = new Customer("John Doe");
+            CustInfo c = new CustInfo("John Doe");
 
-            customer.addRental(new Rental(new Movie("Harry Potter", Movie.REGULAR), 4));
-            customer.addRental(new Rental(new Movie("John Wick", Movie.CHILDRENS), 10));
-            customer.addRental(new Rental(new Movie("The Boult", Movie.NEW_RELEASE), 20));
+            c.addRental(new RentalDetails(new MovieData("Harry Potter", 0), 4));
+            c.addRental(new RentalDetails(new MovieData("John Wick", 2), 10));
+            c.addRental(new RentalDetails(new MovieData("The Boult",1), 20));
 
-            String statement = customer.statement();
+            String actual = c.statement();
 
             Assert.AreEqual("Rental Record for John Doe\n" +
                     "\tHarry Potter\t5\n" +
                     "\tJohn Wick\t12\n" +
                     "\tThe Boult\t60\n" +
                     "Amount owed is 77\n" +
-                    "You earned 4 frequent renter points", statement);
+                    "You earned 4 frequent renter points", actual);
 
         }
     }
