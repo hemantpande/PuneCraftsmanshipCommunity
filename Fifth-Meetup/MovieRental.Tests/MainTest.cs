ac9eb75a962ae671faf5e177e4e3da5475efdedb
Hemant Pande <hemant.pande87@gmail.com>
2019-11-06 00:32:05 +0530

    Removing tests. Will show it at the time of demo

--- Fifth-Meetup/MovieRental.Tests/MainTest.cs
+++ Fifth-Meetup/MovieRental.Tests/MainTest.cs
@@ -7,24 +7,6 @@ namespace MovieRental.Tests
     [TestClass]
     public class MainTest
     {
-        [TestMethod]
-        public void Customer_GenerateTextStatement() 
-        {
-            CustInfo c = new CustInfo("John Doe");
-
-            c.addRental(new RentalDetails(new MovieData("Harry Potter", 0), 4));
-            c.addRental(new RentalDetails(new MovieData("John Wick", 2), 10));
-            c.addRental(new RentalDetails(new MovieData("The Boult",1), 20));
-
-            String actual = c.statement();
-
-            Assert.AreEqual("Rental Record for John Doe\n" +
-                    "\tHarry Potter\t5\n" +
-                    "\tJohn Wick\t12\n" +
-                    "\tThe Boult\t60\n" +
-                    "Amount owed is 77\n" +
-                    "You earned 4 frequent renter points", actual);
-
-        }
+        
     }
 }
